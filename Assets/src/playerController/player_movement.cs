6e4bfc2a07fba0831a59020708bedb27ddae8ddd
Isabedo <99741288+Isabedo@users.noreply.github.com>
2026-05-23 00:48:23 -0500

    assets acomodados

--- Assets/src/playerController/player_movement.cs
+++ Assets/src/playerController/player_movement.cs
@@ -5,25 +5,49 @@ using UnityEngine;
 
 public class player_movement : MonoBehaviour
 {
+    [SerializeField] private float _speed = 5f;
+    [SerializeField] private Rigidbody _rb;
+    private Vector3 _input;
+    private Joystick _joystickMovement;
+    [SerializeField] private float _rotationSpeed = 360f;
 
-    float input_y;
-    float input_x;
-    public float speed;
-
-    // Start is called before the first frame update
     void Start()
     {
-
+        _rb = GetComponent<Rigidbody>();
+        _joystickMovement = FindObjectOfType<Joystick>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        input_y = Input.GetAxis("Vertical");
-        input_x = Input.GetAxis("Horizontal");
-        Vector3 vector3 = new Vector3(input_x, 0, input_y);
-        gameObject.transform.Translate(vector3 * speed * Time.deltaTime);
-
-
+        GatherInput();
+        Look();
     }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void GatherInput() // Obtiene la entrada del joystick para mover al jugador, si no hay joystick no hace nada
+    {
+        if (_joystickMovement == null) return;
+        _input = new Vector3(_joystickMovement.Horizontal, 0, _joystickMovement.Vertical);
+    }
+
+    void Look() // Hace que el jugador mire en la dirección del movimiento, si no hay input no hace nada
+    {
+        if (_input != Vector3.zero)
+        {
+            var relative = (transform.position + _input) - transform.position;
+            var rotation = Quaternion.LookRotation(relative, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, _rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    void Move() // Mueve al jugador en la dirección del input, si no hay input no hace nada
+    {
+        if (_rb == null) return;
+        _rb.MovePosition(transform.position + (transform.forward * _input.magnitude) * _speed * Time.fixedDeltaTime);
+    }
+
+
 }
